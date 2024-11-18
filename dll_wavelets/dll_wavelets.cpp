6dac0e768a4e8edd8f168b62613b4b88a9fd7b3a
bavyikinVA <bavykinvlad752@gmail.com>
2024-11-19 00:21:34 +0300

    update dll

--- dll_wavelets/dll_wavelets.cpp
+++ dll_wavelets/dll_wavelets.cpp
@@ -1,21 +1,28 @@
 ﻿#include "dll_wavelets.h"
 #include <cmath>
 #include <iostream>
+#include <omp.h>
 
 extern "C" {
-    void morlet_wavelet(int num_scales, int rows, int cols, double* data, double* scales, double* result) {
-        for (int scale = 0; scale < num_scales; ++scale) {
-            for (int row = 0; row < rows; ++row) {
-                for (int col = 0; col < cols; ++col) {
+    void morlet_wavelet(int num_scales, int rows, int cols, double* data, double* scales, double* result)
+    {
+        #pragma omp parallel for
+        for (int scale = 0; scale < num_scales; ++scale) 
+        {
+            for (int row = 0; row < rows; ++row) 
+            {
+                double* result_row = result + scale * rows * cols + row * cols;
+                #pragma omp parallel for
+                for (int col = 0; col < cols; ++col) 
+                {
                     double w0 = 0;
                     for (int k = 0; k < cols; ++k) {
                         double t = (double)(k - col) / scales[scale];
                         w0 += data[row * cols + col] * 0.75 * std::exp(-(t * t) / 2) * std::cos(6 * t);
                     }
-                    result[scale * rows * cols + row * cols + col] = w0 / std::sqrt(scales[scale]);
+                    result_row[col] = w0 / std::sqrt(scales[scale]);
                 }
             }
         }
     }
 }
-
--- dll_wavelets/dll_wavelets.h
+++ dll_wavelets/dll_wavelets.h
@@ -1,4 +1,4 @@
 #include <vector>
 #pragma once
-//extern "C" __declspec(dllexport) int sum(int, int);
+
 extern "C" __declspec(dllexport) void morlet_wavelet(int, int, int, double*, double*, double*);
